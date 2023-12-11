2767a71fc27a9ae501895ef3f20f612b569882a7
boranesinc <59020208+boranesin@users.noreply.github.com>
2023-12-12 00:25:17 +0300

    ilk commit umarım

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        //değişkenleri oluşturduk
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        // scanner sınıfını tanittık
+        Scanner inp = new Scanner(System.in);
+
+        //şimdi verileri almaya başlayacağız
+        System.out.print("Lütfen Matematik notunuzu giriniz: ");
+        mat = inp.nextInt();
+        System.out.print("Lütfen Fizik notunuzu giriniz: ");
+        fizik = inp.nextInt();
+        System.out.print("Lütfen Kimya notunuzu giriniz: ");
+        kimya = inp.nextInt();
+        System.out.print("Lütfen Türkçe notunuzu giriniz: ");
+        turkce = inp.nextInt();
+        System.out.print("Lütfen Tarih notunuzu giriniz: ");
+        tarih = inp.nextInt();
+        System.out.print("Lütfen Muzik notunuzu giriniz: ");
+        muzik = inp.nextInt();
+
+        // buraya kadar tanımaları yapıp değişkenlerimiz/dersler e veri girişi sağladık
+        //şimdi burada bu girdilerin toplamını bir değişkene atayıp kontrolleri sağlayacağız
+        int toplam = mat + fizik + kimya + turkce + muzik + tarih ;
+        double ortalama = toplam / 6;
+
+        String yiltekrari = ortalama >= 60 ? "Sınıfı geçti" : "Sınıfta galdın la!";
+        System.out.println(ortalama);
+        System.out.print(yiltekrari);
+    }
+}
